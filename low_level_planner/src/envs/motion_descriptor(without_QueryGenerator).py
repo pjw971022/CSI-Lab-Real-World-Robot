7ccdd7b5fe8742da3ca8c17db7c79deefd94f166
SANGHYUNAHN <shyuni329@gmail.com>
2024-03-11 15:06:12 +0900

    0311 fixed

--- low_level_planner/src/envs/motion_descriptor(without_QueryGenerator).py
+++ low_level_planner/src/envs/motion_descriptor(without_QueryGenerator).py
@@ -2,10 +2,14 @@ import os
 import vertexai
 from vertexai.preview.generative_models import GenerativeModel, Part
 from google.cloud import storage
+import openai
+openai.api_key = "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"
+from openai import OpenAI
 
 location = "asia-northeast3"
 project_id = "gemini-api-415903"
 key_path = "/home/shyuni5/file/CORL2024/Sembot/gemini-api-415903-0f8224218c2c.json"
+api_key = "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"
 
 # Video & img path는 아래와 같다고 가정.
 # img, video 변수를 통해서 각 파일의 이름을 넘겨주면됨.
@@ -22,6 +26,7 @@ Describe the motion of robot arm using the following form:
 
 [start of description]
 
+Action name: 
 Speed: The robot arm should move at a speed of [NUM: 0.0]m/s.
 Required Force: The robot arm should apply a force of [NUM: 0.0] Newtons.
 [optional] Approach object: The robot arm should approach the object at a speed of [NUM: 0.0]m/s.
@@ -78,7 +83,7 @@ def upload_blob(source_file_name, destination_blob_name, bucket_name = 'expert_v
 
 #-----------------------------------------------------------------------------
 
-class MotionDescriptor:
+class MotionDescriptor_WithoutQuery:
     def __init__(self,) -> None:
         self.vision_config = {"max_output_tokens": 800, "temperature": 0.0, "top_p": 1, "top_k": 32}
         self.text_config = {"max_output_tokens": 512, "temperature": 0.0, "top_p": 1}
@@ -89,6 +94,10 @@ class MotionDescriptor:
             },
         ]
         self.model = self.setup(location, project_id)
+
+        self.gpt_assistant_prompt = 'You are a planner of a robot arm for manipulation task.'
+        self.temperature=0.0
+        self.max_tokens=512
 
     def setup(self, location, project_id):
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
@@ -130,35 +139,123 @@ class MotionDescriptor:
         response_1 = self.model.generate_content(contents, generation_config=self.vision_config) #, safety_settings = self.safety_settings
         description_image = response_1.text
 
+        # print(description_image)
+        # print()
+        # print()
+
         contents = [video_file, prompt_demo_extractor_with_instruction ]
         response_2 = self.model.generate_content(contents, generation_config=self.vision_config) #, safety_settings = self.safety_settings
         description_video = response_2.text
+
+        # print(description_video)
+        # print()
+        # print()
 
         contents = ["[Current image description]\n" + description_image + "\n", "[Goal description]\n" + description_video + "\n",  prompt_descriptor_with_instruction]
         response_3 = self.model.generate_content(contents, generation_config=self.text_config) #, safety_settings = self.safety_settings
         return response_3.text
     
+    def gpt4_gen_u2c(self, user_command):
+        prompt_descriptor_with_instruction = prompt_descriptor.format(user_command)
+        message=[{"role": "assistant", "content": self.gpt_assistant_prompt}, {"role": "user", "content": prompt_descriptor_with_instruction}]
+        client = OpenAI(api_key=api_key)
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages = message,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens
+        )
+
+        return response.choices[0].message.content
+
+    
+    def gpt4_gen_o2c(self, img, user_command):
+        prompt_descriptor_with_instruction = prompt_descriptor.format(user_command)
+        prompt_obs_extractor_with_instruction = prompt_obs_extractor.format(user_command)
+        image_uri = upload_blob(image_path+img, img)
+        image_file = Part.from_uri(image_uri, mime_type="image/png")
+
+        contents = [image_file, prompt_obs_extractor_with_instruction ]
+        response_1 = self.model.generate_content(contents, generation_config=self.vision_config) #, safety_settings = self.safety_settings
+        description_image = response_1.text
+
+        message=[{"role": "assistant", "content": self.gpt_assistant_prompt}, {"role": "user", "content": "[Current image description]\n" + description_image + "\n" +  prompt_descriptor_with_instruction}]
+        client = OpenAI(api_key=api_key)
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages = message,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens
+        )
+
+        return response.choices[0].message.content
+
+    def gpt4_gen_d2c(self, video, img, user_command):
+        prompt_descriptor_with_instruction = prompt_descriptor.format(user_command)
+        prompt_obs_extractor_with_instruction = prompt_obs_extractor.format(user_command)
+        prompt_demo_extractor_with_instruction = prompt_demo_extractor.format(user_command)
+        video_uri = upload_blob(video_path+video, video)
+        image_uri = upload_blob(image_path+img, img)
+        video_file = Part.from_uri(video_uri, mime_type="video/mp4")
+        image_file = Part.from_uri(image_uri, mime_type="image/png")
+
+        contents = [image_file, prompt_obs_extractor_with_instruction ]
+        response_1 = self.model.generate_content(contents, generation_config=self.vision_config) #, safety_settings = self.safety_settings
+        description_image = response_1.text
+
+        contents = [video_file, prompt_demo_extractor_with_instruction ]
+        response_2 = self.model.generate_content(contents, generation_config=self.vision_config) #, safety_settings = self.safety_settings
+        description_video = response_2.text
+
+        message=[{"role": "assistant", "content": self.gpt_assistant_prompt}, {"role": "user", "content": "[Current image description]\n" + description_image + "\n"+ "[Goal description]\n" + description_video + "\n" + prompt_descriptor_with_instruction}]
+        client = OpenAI(api_key=api_key)
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages = message,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens
+        )
+
+        return response.choices[0].message.content
+
+
+
 
 #-----------------------------------------------------------------------------
 
 # #TEST
 
-descriptor = MotionDescriptor()
+descriptor = MotionDescriptor_WithoutQuery()
 
 # Test gemini_gen_u2c
 user_command = "How can I accomplish the task of pouring liquid from a red cup into a maroon cup?"
 
 
-print("Testing gemini_gen_u2c:")
-print(descriptor.gemini_gen_u2c(user_command))
+# print("Testing gemini_gen_u2c:")
+# print(descriptor.gemini_gen_u2c(user_command))
 
-# Test gemini_gen_o2c
-print("\nTesting gemini_gen_o2c:")
-img = "front_rgb.png"
-print(descriptor.gemini_gen_o2c(img, user_command))
+# # Test gemini_gen_o2c
+# print("\nTesting gemini_gen_o2c:")
+# img = "front_rgb.png"
+# print(descriptor.gemini_gen_o2c(img, user_command))
 
 # Test gemini_gen_d2c
-print("\nTesting gemini_gen_d2c:")
-img = "front_rgb.png"
-video = "demo_video.mp4"
-print(descriptor.gemini_gen_d2c(video, img, user_command))
+# print("\nTesting gemini_gen_d2c:")
+# img = "front_rgb.png"
+# video = "demo_video.mp4"
+# print(descriptor.gemini_gen_d2c(video, img, user_command))
+
+# print("Testing gpt4_gen_u2c:")
+# print(descriptor.gpt4_gen_u2c(user_command))
+
+# print("\nTesting gpt4_gen_o2c:")
+# img = "front_rgb.png"
+# print(descriptor.gpt4_gen_o2c(img, user_command))
+
+# # Test gemini_gen_d2c
+# print("\nTesting gpt4_gen_d2c:")
+# img = "front_rgb.png"
+# video = "demo_video.mp4"
+# print(descriptor.gpt4_gen_d2c(video, img, user_command))
+
+
